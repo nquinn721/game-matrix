81a035e1bc6adb11864ff6ea167e87b2cd0ca79d
Nathan <nquinn@medonesystems.com>
2020-12-08 16:09:40 -0500

    update

--- packages/game/server/src/app.controller.ts
+++ packages/game/server/src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Game } from 'game-matrix';
-
+import {} from 'game-matrix';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const g = new Game({});
     return this.appService.getHello();
   }
 }
